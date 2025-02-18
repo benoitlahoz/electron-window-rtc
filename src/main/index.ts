4d6c2f0ee4128a636865cfbcaeda32cdf0c13200
benoitlahoz <lahoz.ben@gmail.com>
2025-02-18 17:10:55 +0100

    Updated main events hub (signallng).

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,25 +1,63 @@
-import type { BrowserWindow } from 'electron';
+import type { BrowserWindow, IpcMainInvokeEvent } from 'electron';
+import { ipcMain } from 'electron';
+import { WindowRTCChannels } from '../common/channels';
+import { ForwardMessageDTO } from '../common/dto';
 
 export interface RegisteredWindow {
   name: string;
   window: BrowserWindow;
 }
 
-class _WindowMessageHub {
+class _WindowRTCMain {
   /**
    * Singleton instance.
    */
-  private static instance: _WindowMessageHub;
+  private static instance: _WindowRTCMain | null = null;
 
   /**
    * An `Array` of registered `BrowserWindow` with unique names.
    */
-  public readonly windows: RegisteredWindow[] = [];
+  public readonly registeredWindows: RegisteredWindow[] = [];
+
+  // These will receive instance corresponding methods bound to 'this'.
+  private _getOwnNameCallback!: (event: IpcMainInvokeEvent) => Promise<string>;
+  private _getWindowsCallback!: () => Promise<string[]>;
+  private _forwardMessageCallback!: (
+    event: IpcMainInvokeEvent,
+    data: ForwardMessageDTO
+  ) => Promise<undefined | Error>;
 
   constructor(private logger = console) {
-    if (_WindowMessageHub.instance) return _WindowMessageHub.instance;
+    if (_WindowRTCMain.instance) return _WindowRTCMain.instance;
 
-    _WindowMessageHub.instance = this;
+    // Bind callbacks.
+    this._getOwnNameCallback = this.getOwnNameCallback.bind(this);
+    this._getWindowsCallback = this.getWindowsCallback.bind(this);
+    this._forwardMessageCallback = this.forwardMessageCallback.bind(this);
+
+    ipcMain.handle(
+      WindowRTCChannels.GetOwnWindowName,
+      this._getOwnNameCallback
+    );
+
+    ipcMain.handle(
+      WindowRTCChannels.GetRegisteredWindows,
+      this._getWindowsCallback
+    );
+
+    ipcMain.handle(WindowRTCChannels.Signal, this._forwardMessageCallback);
+
+    _WindowRTCMain.instance = this;
+  }
+
+  /**
+   * Dispose the singleton.
+   */
+  public dispose(): void {
+    ipcMain.removeHandler(WindowRTCChannels.GetOwnWindowName);
+    ipcMain.removeHandler(WindowRTCChannels.GetRegisteredWindows);
+    ipcMain.removeHandler(WindowRTCChannels.Signal);
+    _WindowRTCMain.instance = null;
   }
 
   /**
@@ -30,7 +68,7 @@ class _WindowMessageHub {
    */
   public register(name: string, window: BrowserWindow): void {
     const cleanName = name.trim();
-    const existing = this.windows.find(
+    const existing = this.registeredWindows.find(
       (registered: RegisteredWindow) =>
         registered.name === cleanName || registered.window === window
     );
@@ -39,9 +77,20 @@ class _WindowMessageHub {
       throw new Error(`Window with name '${cleanName}' is already registered.`);
     }
 
-    this.windows.push({
+    this.registeredWindows.push({
       name: cleanName,
       window,
+    });
+
+    window.on('close', () => {
+      for (const registered of this.registeredWindows) {
+        registered.window.webContents.send(WindowRTCChannels.Signal, {
+          channel: 'leave',
+          sender: cleanName,
+          receiver: registered.name,
+          payload: undefined,
+        });
+      }
     });
   }
 
@@ -52,7 +101,7 @@ class _WindowMessageHub {
    */
   public unregister(name: string): void {
     const cleanName = name.trim();
-    const existing = this.windows.find(
+    const existing = this.registeredWindows.find(
       (registered: RegisteredWindow) => registered.name === cleanName
     );
 
@@ -62,10 +111,53 @@ class _WindowMessageHub {
       );
     }
 
-    const index = this.windows.indexOf(existing);
-    this.windows.splice(index, 1);
+    const index = this.registeredWindows.indexOf(existing);
+    this.registeredWindows.splice(index, 1);
+  }
+
+  private async getOwnNameCallback(event: IpcMainInvokeEvent): Promise<string> {
+    const existing = this.registeredWindows.find(
+      (registered: RegisteredWindow) =>
+        registered.window.webContents === event.sender
+    );
+
+    if (!existing) {
+      throw new Error(
+        `Unable to find window own name. It may not be properly registered.`
+      );
+    }
+
+    return existing.name;
+  }
+
+  /**
+   * Callback for `WindowRTCChannels.GetWindows`
+   * @returns { Promise<string[]>} The registered windows names.
+   */
+  private async getWindowsCallback(): Promise<string[]> {
+    return this.registeredWindows.map(
+      (window: RegisteredWindow) => window.name
+    );
+  }
+
+  private async forwardMessageCallback(
+    _event: IpcMainInvokeEvent,
+    data: ForwardMessageDTO
+  ): Promise<undefined | Error> {
+    const existing = this.registeredWindows.find(
+      (registered: RegisteredWindow) => registered.name === data.receiver
+    );
+
+    if (!existing) {
+      return new Error(
+        `Receiver window with name '${data.receiver}' could not be found.`
+      );
+    }
+
+    existing.window.webContents.send(WindowRTCChannels.Signal, data);
+    return;
   }
 }
 
-const singleton = new _WindowMessageHub();
-export { singleton as WindowMessageHub };
+const singleton = new _WindowRTCMain();
+export { singleton as WindowRTCMain };
